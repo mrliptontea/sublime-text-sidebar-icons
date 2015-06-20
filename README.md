1364daa5d53697c90f11d4630a12ce4994a1bc20
mrliptontea <rayman17@gmail.com>
2015-06-20 16:43:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,72 @@
+# Sublime Text sidebar icons
+
+> The collection of file type icons for Sublime Text editor.
+
+Please note that icons included in this repository are best suited with a dark background but you can [customize](https://github.com/mrliptontea/sublime-text-sidebar-icons#customizing-the-icons) them.
+
+
+## Installation
+
+The purpose of this collection is to override the default icons that come with [themes]. Unfortunately, this prevents from creating a package that could be installed via [Package Control].
+
+To install the icons you'll have to clone this repo to your [Packages] folder and rename the folder's name to match your current theme's package (folder) name, like so:
+
+    git clone https://github.com/mrliptontea/sublime-text-sidebar-icons [folder name]
+
+For example, I use Predawn theme, so my `[folder name]` is `Predawn`.
+
+Other themes that I tested and folder name you should use:
+
+* Sublime's default theme - `Theme - Default`,
+* [Afterglow] - `Theme - Afterglow`,
+* [Aprosopo] - `Theme - Aprosopo`,
+* [Cobalt2] - `Theme - Cobalt2`,
+* [Coffee] - `Theme - Coffee`,
+* [Cyanide] - `Theme - Cyanide`,
+* [Flatgrammer] - `Theme - Flatgrammer`,
+* [Fox] - `Theme - Fox`,
+* [Material] - `Material Theme`,
+* [Predawn] - `Predawn`,
+* [SoDaReloaded] - `Theme - SoDaReloaded`,
+* ...and so on.
+
+
+## Customizing the icons
+
+In `src` you'll find `file_type_icons.psd` file which contains all the icons. You can modify the however you want, all you need is Photoshop CS6 or later (but it might work in older versions).
+
+Also there's simple Photoshop script `export-icons.jsx` that looks for layer groups with names ending with '.png' and exports them to `../icons` folder. You can run it by clicking File → Scripts → Browse... and selecting the script.
+
+
+## Further notes
+
+For managing themes I suggest the [Themr] package, it makes changing themes and customizing them much easier.
+
+If you want me to add some icons or have any suggestions, please add [new issue](https://github.com/mrliptontea/sublime-text-sidebar-icons/issues/new).
+Feel free to make new icons and send the full `file_type_icons.psd` if you want to share.
+
+Most of the icons I took from the great [devicons] and [devicon] sets, some are slightly modified and some are I drew myself.
+
+## License
+
+Under [MIT Licence](https://github.com/mrliptontea/sublime-text-sidebar-icons/blob/master/LICENSE).
+
+*I do not own rights for any of the logos or other images used, they belong to the rightful owners.*
+
+
+[Package Control]: https://packagecontrol.io/
+[Packages]: http://docs.sublimetext.info/en/latest/basic_concepts.html#the-packages-directory
+[Themr]: https://packagecontrol.io/packages/Themr
+[themes]: https://packagecontrol.io/browse/labels/theme
+[Afterglow]: https://packagecontrol.io/packages/Theme%20-%20Afterglow
+[Aprosopo]: https://packagecontrol.io/packages/Theme%20-%20Aprosopo
+[Cobalt2]: https://packagecontrol.io/packages/Theme%20-%20Cobalt2
+[Coffee]: https://packagecontrol.io/packages/Theme%20-%20Coffee
+[Cyanide]: https://packagecontrol.io/packages/Theme%20-%20Cyanide
+[Flatgrammer]: https://packagecontrol.io/packages/Theme%20-%20Flatgrammer
+[Fox]: https://packagecontrol.io/packages/Theme%20-%20Fox
+[Material]: https://packagecontrol.io/packages/Material%20Theme
+[Predawn]: https://packagecontrol.io/packages/Predawn
+[SoDaReloaded]: https://packagecontrol.io/packages/Theme%20-%20SoDaReloaded
+[devicons]: http://vorillaz.github.io/devicons/
+[devicon]: http://devicon.fr/
---
+++ src/export-icons.jsx
@@ -0,0 +1,160 @@
+// $.level = 2;
+/**
+ * Script for exporting icons to separate PNG files.
+ *
+ * Grzegorz Rajchman (c) 2015
+ * Based on script by Damien van Holten:
+ * http://www.damienvanholten.com/blog/export-groups-to-files-photoshop/
+ */
+
+function main()
+{
+    if (!documents.length) return;
+
+    var iconBaseSize = 16;
+    var iconRetinaSuffix = '@2x';
+    var iconNamePrefix = 'file_type_';
+    var outputFolder = '../icons';
+
+    var srcDocument = activeDocument;
+    var srcPath = activeDocument.path;
+    var outPath = srcPath + '/' + outputFolder;
+    var outFolder = new Folder(outPath);
+
+    if (!outFolder.exists) {
+        outFolder.create();
+    }
+
+    var layerSets = getLayerSets();
+    scanLayerSets(layerSets);
+
+    function getLayerSets()
+    {
+        var all = confirm('Do you wish to export only selected icons (Yes) or all of them (No)?');
+
+        if (all) {
+            return getSelectedLayerSets();
+        }
+
+        return getAllLayerSets();
+    }
+
+    function getAllLayerSets()
+    {
+        return srcDocument.layerSets;
+    }
+
+    function getSelectedLayerSets()
+    {
+        var layerArray = [];
+
+        if (!srcDocument.activeLayer.layers) {
+            return layerArray;
+        }
+
+        var actionDesc = new ActionDescriptor();
+
+        var selRef = new ActionReference();
+        selRef.putClass(stringIDToTypeID('layerSection'));
+        actionDesc.putReference(charIDToTypeID('null'), selRef);
+
+        var layerRef = new ActionReference();
+        layerRef.putEnumerated(charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+        actionDesc.putReference(charIDToTypeID('From'), layerRef);
+
+        // Step 1. Temporarily group all the selected layers
+        executeAction(charIDToTypeID('Mk  '), actionDesc, DialogModes.NO);
+
+        // Step 2. Store the current active layer's layers
+        var tempLayerSet = srcDocument.activeLayer.layers;
+        var layerIndex;
+
+        // Step 3. Filter through the layers and store them to the results array
+        for (layerIndex = 0; layerIndex < tempLayerSet.length; layerIndex++) {
+            // Check only for LayerSets (Groups)
+            if (tempLayerSet[layerIndex].hasOwnProperty('layers')) {
+                layerArray.push(tempLayerSet[layerIndex]);
+            }
+        }
+
+        // Step 4. Undo the temporary group
+        executeAction(charIDToTypeID('undo'), undefined, DialogModes.NO);
+
+        return layerArray;
+    }
+
+    function scanLayerSets(layerSets)
+    {
+        if (!layerSets.length) {
+            alert('Please select at least one layer group.');
+        }
+
+        // A hack to avoid exporting all the groups as the first image
+        var dummy = srcDocument.layers.getByName('Background');
+        srcDocument.activeLayer = dummy;
+
+        var layerIndex;
+
+        for (layerIndex = 0; layerIndex < layerSets.length; layerIndex++) {
+            var groupName = layerSets[layerIndex].name;
+
+            if (groupName.substr(-4) == '.png') {
+                var iconScale = groupName.substr(-7, 3) == iconRetinaSuffix ? 2 : 1;
+                var layer = srcDocument.layers.getByName(groupName);
+
+                saveLayer(layer, groupName, iconScale);
+            }
+        }
+    }
+
+    function saveLayer(layer, groupName, iconScale)
+    {
+        iconScale = iconScale || 1;
+
+        var fileName = iconNamePrefix + groupName;
+        var filePath = File(outPath + '/' + fileName);
+        var pxSize = UnitValue(iconBaseSize * iconScale, 'px');
+
+        activeDocument.activeLayer = layer;
+        duplicateToNewFile(groupName);
+
+        activeDocument.mergeVisibleLayers();
+        activeDocument.trim(TrimType.TRANSPARENT, true, true, true, true);
+        activeDocument.resizeCanvas(pxSize, pxSize);
+
+        savePNG(filePath);
+        app.activeDocument.close(SaveOptions.DONOTSAVECHANGES);
+    }
+
+    function duplicateToNewFile(name)
+    {
+        var desc143 = new ActionDescriptor();
+        var ref73 = new ActionReference();
+
+        ref73.putClass(charIDToTypeID('Dcmn'));
+        desc143.putReference(charIDToTypeID('null'), ref73);
+        desc143.putString(charIDToTypeID('Nm  '), name);
+
+        var ref74 = new ActionReference();
+        ref74.putEnumerated(charIDToTypeID('Lyr '), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+        desc143.putReference(charIDToTypeID('Usng'), ref74);
+        executeAction(charIDToTypeID('Mk  '), desc143, DialogModes.NO);
+    }
+
+    function savePNG(saveFile)
+    {
+        var pngOpts = new ExportOptionsSaveForWeb();
+
+        pngOpts.format = SaveDocumentType.PNG;
+        pngOpts.PNG8 = false;
+        pngOpts.transparency = true;
+        pngOpts.interlaced = false;
+        pngOpts.quality = 100;
+
+        activeDocument.exportDocument(
+            new File(saveFile), ExportType.SAVEFORWEB, pngOpts
+       );
+    }
+}
+
+main();
